efeea50cb99a79572ba52c23298fbdbeca776420
ZBrian199 <brianzelada1999@gmail.com>
2023-03-16 23:19:12 -0300

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Header } from './components/Header';
+import { Icon } from './components/Prueba1';
 import { DataProvider } from './context';
 
 export const App = () => {
 	return (
 		<DataProvider>
-			<Header />
+			<Icon width='500px' height='500px' />
 		</DataProvider>
 	);
 };
---
+++ src/components/Prueba1.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+export const Icon = (props) => {
+	return (
+		<svg
+			width='1em'
+			height='1em'
+			viewBox='0 0 297 309'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+			{...props}
+		>
+			<g opacity={0.76}>
+				<path fill='#261D1D' d='M113.471 0l36.647 42.158-76.47 66.475L37 66.475z' />
+				<path fill='#AA236D' d='M150.12 66.633l36.647 42.158-76.471 66.475-36.648-42.158z' />
+				<path fill='#F21368' d='M186.766 133.267l36.647 42.158-76.47 66.475-36.648-42.158z' />
+				<path fill='#F49393' d='M223.413 199.9l36.647 42.158-76.47 66.475-36.648-42.158z' />
+			</g>
+		</svg>
+	);
+};
+
