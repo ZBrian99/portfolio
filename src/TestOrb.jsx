0898849b2006f7e8d8840df0ef9361752b642613
ZBrian99 <brianzelada1999@gmail.com>
2024-04-13 14:55:48 -0300

    iconos rotan en base al campo de vision

--- src/TestOrb.jsx
+++ src/TestOrb.jsx
@@ -10,7 +10,8 @@ const PlanetContainer = styled.div`
 	background-image: url('src/assets/images/background.png');
 	background-repeat: no-repeat;
 	background-attachment: fixed;
-	/* background-size: cover; */
+	background-size: cover;
+	background-position: center;
 `;
 
 const Sun = styled(motion.div)`
@@ -65,7 +66,7 @@ const Planet = styled(motion.div)`
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	background-color: #1180db;
+	/* background-color: #1180db; */
 	/* margin: calc(-30vw / 2) auto; */
 	margin: calc(-${({ planetSize }) => planetSize} / 2) auto;
 `;
@@ -74,69 +75,101 @@ const PlanetIcon = styled.img`
 	height: 80%;
 	object-fit: cover;
 `;
+const Line = styled.div`
+	position: fixed;
+	width: 300%; /* Doble del ancho de la ventana */
+	height: 5px; /* Grosor de la línea */
+	background-color: red;
+	bottom: 0; /* Fijar a la parte inferior */
+	left: 0; /* Fijar a la parte izquierda */
+	transform-origin: center left;
+	transform: rotate(-${(props) => props.angle}deg);
+  z-index: 10;
+`;
+
+const getScreenAngle = () => {
+	const screenWidth = window.innerWidth;
+	const screenHeight = window.innerHeight;
+	const angleRadians = Math.atan2(screenHeight, screenWidth);
+	const angleDegrees = angleRadians * (180 / Math.PI);
+	return angleDegrees;
+};
+
 export const SistemaSolar = () => {
 	const planetsData = [
-		{ orbitSize: '30vw', planetSize: '3vw', rotateTime: 5, startAngle: 45, endAngle: 65 },
-		{ orbitSize: '50vw', planetSize: '9vw', rotateTime: 10, startAngle: 45, endAngle: 65 },
-		{ orbitSize: '70vw', planetSize: '13vw', rotateTime: 7, startAngle: 45, endAngle: 65 },
-		{ orbitSize: '90vw', planetSize: '20vw', rotateTime: 8, startAngle: 45, endAngle: 65 },
-		{ orbitSize: '110vw', planetSize: '8vw', rotateTime: 6, startAngle: 45, endAngle: 65 },
-		{ orbitSize: '130vw', planetSize: '15vw', rotateTime: 9, startAngle: 45, endAngle: 65 },
-		{ orbitSize: '150vw', planetSize: '5vw', rotateTime: 4, startAngle: 45, endAngle: 65 },
-		{ orbitSize: '170vw', planetSize: '12vw', rotateTime: 11, startAngle: 45, endAngle: 65 },
-		{ orbitSize: '190vw', planetSize: '18vw', rotateTime: 7, startAngle: 45, endAngle: 65 },
+		{ orbitSize: '40vw', planetSize: '4vw', rotateTime: 15, startAngle: 5, endAngle: 8 },
+		{ orbitSize: '53vw', planetSize: '6vw', rotateTime: 10, startAngle: 5, endAngle: 8 },
+		{ orbitSize: '70vw', planetSize: '10vw', rotateTime: 17, startAngle: 5, endAngle: 8 },
+		{ orbitSize: '95vw', planetSize: '13vw', rotateTime: 18, startAngle: 5, endAngle: 8 },
+		{ orbitSize: '115vw', planetSize: '8vw', rotateTime: 16, startAngle: 5, endAngle: 8 },
+		{ orbitSize: '133vw', planetSize: '9vw', rotateTime: 19, startAngle: 5, endAngle: 8 },
+		{ orbitSize: '150vw', planetSize: '5vw', rotateTime: 14, startAngle: 5, endAngle: 8 },
+		{ orbitSize: '170vw', planetSize: '12vw', rotateTime: 11, startAngle: 5, endAngle: 8 },
+		{ orbitSize: '190vw', planetSize: '10vw', rotateTime: 17, startAngle: 5, endAngle: 8 },
 	];
-
-	const [direction, setDirection] = useState(true);
-	const controls = useAnimation();
-
+	const [angle, setAngle] = useState(90);
 	useEffect(() => {
-		const startAnimation = async () => {
-			await controls.start({
-				rotate: direction ? 65 : 45,
-				transition: {
-					duration: 5,
-					ease: 'easeInOut',
-				},
-			});
-			setDirection(!direction);
+		setAngle(getScreenAngle());
+		const handleResize = () => {
+			setAngle(getScreenAngle());
 		};
-		startAnimation();
-	}, [controls, direction]);
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+	console.log(angle);
 
 	return (
-		<PlanetContainer>
-			<Sun
-				animate={{
-					rotate: -360,
-				}}
-				transition={{
-					duration: 60,
-					ease: 'linear',
-					repeat: Infinity,
-				}}
-				sunSize={'30vw'}
-			/>
-			{planetsData.map((planet, index) => (
-				<PlanetOrbit key={index} orbitSize={planet.orbitSize}>
-					<PlanetRotation
-						initial={{ rotate: 0 }}
-						animate={{
-							rotate: [planet.endAngle, planet.startAngle],
-						}}
-						transition={{
-							duration: planet.rotateTime,
-							ease: 'easeInOut',
-							repeat: Infinity,
-							repeatType: 'reverse',
-						}}
-					>
-						<Planet planetSize={planet.planetSize}>
-							<PlanetIcon src='src/assets/icons/css.svg' alt='' />
-						</Planet>
-					</PlanetRotation>
-				</PlanetOrbit>
-			))}
-		</PlanetContainer>
+		<>
+			<Line angle={10 + angle} />
+			<Line angle={-20 + angle} />
+			<PlanetContainer>
+				<Sun
+					animate={{
+						rotate: -360,
+					}}
+					transition={{
+						duration: 60,
+						ease: 'linear',
+						repeat: Infinity,
+					}}
+					sunSize={'30vw'}
+				/>
+				{planetsData.map((planet, index) => (
+					<PlanetOrbit key={index} orbitSize={planet.orbitSize}>
+						<PlanetRotation
+							animate={{
+								rotate: [100 - angle, 90 - angle],
+								// rotate: [planet.endAngle + angle, planet.startAngle + angle],
+							}}
+							transition={{
+								duration: planet.rotateTime,
+								ease: 'easeInOut',
+								repeat: Infinity,
+								repeatType: 'reverse',
+							}}
+						>
+							<Planet
+								animate={{
+									// rotate: [0, 0],
+									rotate: [(100 - angle) * -1, (90 - angle) * -1],
+								}}
+								transition={{
+									duration: planet.rotateTime,
+									ease: 'easeInOut',
+									repeat: Infinity,
+									repeatType: 'reverse',
+								}}
+								planetSize={planet.planetSize}
+							>
+								<PlanetIcon src='src/assets/icons/sass.webp' alt='' />
+							</Planet>
+						</PlanetRotation>
+					</PlanetOrbit>
+				))}
+			</PlanetContainer>
+		</>
 	);
 };
