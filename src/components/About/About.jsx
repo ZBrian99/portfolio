d07a8041ce45e182483a2c391fcb7479c50354d1
ZBrian99 <brianzelada1999@gmail.com>
2024-06-01 08:38:38 -0300

    update data

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -107,7 +107,7 @@ export const About = () => {
 					</AboutDescription>
 					<Skills />
 					<GlowButtonAlt
-						href='https://drive.google.com/file/d/1lptshUrY8o86VVjv5BAG1XpLjERGjsNs/view?usp=sharing'
+						href='https://drive.google.com/file/d/1Tx12_CUTcqeT5JMl8Q3ArLxtfXA-3UVv/view?usp=sharing'
 						target='_blank'
 						rel='noreferrer'
 					>
